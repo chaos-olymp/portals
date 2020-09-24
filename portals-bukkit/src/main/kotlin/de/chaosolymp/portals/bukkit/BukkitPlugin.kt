6fb00d15f7c219773466e75b16e4d34c52cfb820
Robin Lindner <robin@deeprobin.de>
2020-09-24 19:18:42 +0200

    Added bukkit config & enhanced create statement

--- portals-bukkit/src/main/kotlin/de/chaosolymp/portals/bukkit/BukkitPlugin.kt
+++ portals-bukkit/src/main/kotlin/de/chaosolymp/portals/bukkit/BukkitPlugin.kt
@@ -15,18 +15,23 @@ import net.md_5.bungee.api.ChatColor
 import net.md_5.bungee.api.ChatMessageType
 import net.md_5.bungee.api.chat.ComponentBuilder
 import org.bukkit.block.Block
+import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 import java.awt.Color
+import java.io.File
 
 class BukkitPlugin: JavaPlugin() {
 
     private var worldGuard: WorldGuard? = null
 
+    private lateinit var configFile: File
+    private lateinit var config: YamlConfiguration
     private lateinit var pluginCommunicationListener: PluginCommunicationListener
 
     override fun onEnable() {
         this.pluginCommunicationListener = PluginCommunicationListener(this)
+        this.initConfig()
         this.server.pluginManager.registerEvents(PortalListener(this),this)
         this.server.messenger.registerIncomingPluginChannel(this, "BungeeCord", this.pluginCommunicationListener)
         this.server.messenger.registerOutgoingPluginChannel(this, "BungeeCord")
@@ -36,8 +41,22 @@ class BukkitPlugin: JavaPlugin() {
         }
     }
 
+    private fun initConfig() {
+        configFile = File(dataFolder, "messages.yml")
+        if(!configFile.exists()) {
+            configFile.createNewFile()
+            config = YamlConfiguration()
+            config.addDefault("messages.sneak-to-tp", "> Schleiche um dich zu teleportieren <")
+            config.save(configFile)
+            this.logger.info("Default configuration ${configFile.name} created!")
+        } else {
+            config = YamlConfiguration.loadConfiguration(configFile)
+            this.logger.info("Config ${configFile.name} successfully loaded!")
+        }
+    }
+
     internal fun handlePortalAppearance(player: Player) {
-        val builder = ComponentBuilder("> Schleiche um dich zu teleportieren <") // TODO: multi lang
+        val builder = ComponentBuilder(config.getString("messages.sneak-to-tp"))
         builder.color(ChatColor.of(Color(155, 89, 182)))
         builder.font("uniform")
         player.spigot().sendMessage(ChatMessageType.ACTION_BAR, *builder.create())
@@ -48,7 +67,6 @@ class BukkitPlugin: JavaPlugin() {
         val x = block.location.blockX
         val y = block.location.blockY
         val z = block.location.blockZ
-
         val output = ByteStreams.newDataOutput(world.length + 54)
         val uuid = UUIDUtils.getBytesFromUUID(player.uniqueId)
         output.writeUTF("portals:authorize_teleport")
