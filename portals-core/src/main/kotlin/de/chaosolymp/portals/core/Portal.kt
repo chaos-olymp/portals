45f4d9b8481db8b67fc084848774e37922dc31b2
Robin Lindner <robin.lindner1@t-online.de>
2021-11-27 13:31:24 +0100

    Remove unused methods

--- portals-core/src/main/kotlin/de/chaosolymp/portals/core/Portal.kt
+++ portals-core/src/main/kotlin/de/chaosolymp/portals/core/Portal.kt
@@ -3,7 +3,4 @@ package de.chaosolymp.portals.core
 import java.sql.Timestamp
 import java.util.*
 
-data class Portal(val id: Int, val owner: UUID?, val name: String, val displayName: String?, val public: Boolean, val created: Timestamp, val updated: Timestamp = created, val server: String, val world: String, val x: Int, val y: Int, val z: Int, val link: Int?) {
-    fun isLinked() = this.link != null
-    fun isServerPortal() = this.owner == null
-}
+data class Portal(val id: Int, val owner: UUID?, val name: String, val displayName: String?, val public: Boolean, val created: Timestamp, val updated: Timestamp = created, val server: String, val world: String, val x: Int, val y: Int, val z: Int, val link: Int?)
